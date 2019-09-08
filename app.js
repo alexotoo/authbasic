20e3ae277ab43e7b3d75bfd2ecfdd2074c1dfdb3
abraham otoo <abrahamotoo@abrahams-MacBook-Pro.local>
2019-09-08 11:46:43 +0200

    mongoose encryption added

--- app.js
+++ app.js
@@ -1,53 +1,76 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
-const app = express()
+const app = express();
 
-
-app.use(express.static("public"))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}))
+  })
+);
 
-mongoose.connect('mongodb+srv://alex:' + "n4K5ZPo1LXpql1fl" + '@api-shop-b9ere.mongodb.net/userDB', {
+mongoose.connect(
+  "mongodb+srv://alex:" +
+    "n4K5ZPo1LXpql1fl" +
+    "@api-shop-b9ere.mongodb.net/userDB",
+  {
     useNewUrlParser: true
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
 });
 
-const userSchema = {
-    email: String,
-    password: String
-}
+const secret = "forsecurity_reason_PlusW";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
-    res.render("home")
-})
+  res.render("home");
+});
 
 app.get("/login", (req, res) => {
-    res.render("login")
-})
+  res.render("login");
+});
 
 app.get("/register", (req, res) => {
-    res.render("register")
-})
+  res.render("register");
+});
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-    newUser.save((err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render("secrets")
-        }
-    })
-})
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  });
+});
 
 module.exports = app;
