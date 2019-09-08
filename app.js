94da36e0ddf8b6c584d49c107a33f9cd7601ad46
abraham otoo <abrahamotoo@abrahams-MacBook-Pro.local>
2019-09-08 12:23:14 +0200

    dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,69 +9,73 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 mongoose.connect(
-  "mongodb+srv://alex:" +
-    "n4K5ZPo1LXpql1fl" +
-    "@api-shop-b9ere.mongodb.net/userDB",
-  {
-    useNewUrlParser: true
-  }
+    process.env.DBUSER +
+    process.env.DBPW +
+    "@api-shop-b9ere.mongodb.net/userDB", {
+        useNewUrlParser: true
+    }
 );
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String
+    email: String,
+    password: String
 });
 
-const secret = "forsecurity_reason_PlusW";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+const secret = process.env.SECRET
+userSchema.plugin(encrypt, {
+    secret: secret,
+    encryptedFields: ["password"]
+});
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+    res.render("login");
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+    res.render("register");
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(err => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(err => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-  User.findOne({ email: username }, (err, foundUser) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      }
-    }
-  });
+    User.findOne({
+        email: username
+    }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    });
 });
 
 module.exports = app;
