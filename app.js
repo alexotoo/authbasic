f0ef71b9fd0ba6273bad42864461949dd892b8ed
abraham otoo <abrahamotoo@abrahams-MacBook-Pro.local>
2019-09-15 09:06:11 +0200

    google Oauth stra added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
 
@@ -38,16 +40,43 @@ mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+})
+
+//google auth strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3500/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  function (accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({
+      googleId: profile.id
+    }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 //routes
@@ -55,6 +84,21 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }),
+  function (req, res) {
+    // Successful authentication, redirect the secrets page.
+    res.redirect("/secrets");
+  });
+
 
 app.get("/login", (req, res) => {
   res.render("login");
